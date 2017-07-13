de4a608fd5bd4ede806b96094dfa1e0614f0f169
Ryan Nunez and Stephanie Mayer <stephanie@>
2017-07-13 14:39:09 -0700

    add local variable and readme

---
+++ README.md
@@ -0,0 +1,50 @@
+_{Romans}_
+
+#### _{Webpage with interactive form}, {13 July 2017}_
+
+#### By _**{Ryan Nunez & Stephanie Mayer}**_
+
+## Description
+
+_{The site was developed to create an application that converts an integer to a roman numeral based on a users input. Designed as a future resource of good coding practices for spacing, indentation, tag placement and uses. Proper use and spacing of HTML markup block and inline elements and CSS and Javascript. Some of the items used were forms, buttons. Its purpose and usage was simple information, interactivity, styles, and the base of a personal portfolio for future employment opportunities.}_
+
+## Specifications
+_Specs from Romans
+
+*The program should return when a number exceeds 3999 is please enter smaller number.
+ Input Example: 4000
+ Output Example: alert
+*The program should return 'result' for numbers less than 3999.
+ Input Example: 210
+ Output Example: CCX
+
+## Setup/Installation Requirements
+
+* _Create a local version of Romans_
+* _clone repository from github at url: https://github.com/RxNunez/Romans.git
+* _launch index.html file in the browser of choice_
+* _git clone https://github.com/RxNunez/Romans.git
+
+* _Create a local version of RomanNumerals_
+* _clone repository from github at url: https://github.com/smayer0926/romanNumerals
+* _launch index.html file in the browser of choice_
+* _git clone https://github.com/smayer0926/romanNumerals
+
+
+_{The cloning will create the img source directory with required jpg files, in addition to index.html.}_
+
+## Known Bugs
+
+_{There are no known issues.}_
+
+## Support and contact details
+
+_{Email nunez.ryan@gmail.com or smayer0926@gmail.com with any questions or problems or recommendations.}_
+
+## Technologies Used
+
+_{HTML/CSS/JavaScript/JQuery}_
+
+### License
+
+*{This software is licensed under MIT.}*
--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,58 @@
 //Business Logic
-$(function(){
-var convertArray = []
 
 function convert(num) {
-  if (num > 3999){
-    alert("Please enter a smaller number!")
-  } else if (num >= 1000){
-    convertArray.push("M");
-    convert(num - 1000);
-  } else if (num >= 900){
-    convertArray.push("CM");
-    convert(num - 900);
-  } else if (num >= 500){
-    convertArray.push("D");
-    convert(num - 500);
-  } else if (num >= 400){
-    convertArray.push("CD");
-    convert(num - 400);
-  } else if (num >= 100){
-    convertArray.push("C");
-    convert(num - 100);
-  } else if (num >= 90){
-    convertArray.push("XC");
-    convert(num - 90);
-  } else if (num >= 50){
-    convertArray.push("L");
-    convert(num - 50);
-  } else if (num >= 40){
-    convertArray.push("XL");
-    convert(num - 40);
-  } else if (num >= 10){
-    convertArray.push("X");
-    convert(num - 10);
-  } else if (num >= 9){
-    convertArray.push("IX");
-    convert(num - 9);
-  } else if (num >= 5){
-    convertArray.push("V");
-    convert(num - 5);
-  } else if (num >= 4){
-    convertArray.push("IV");
-    convert(num - 4);
-  }else if (num >= 1){
-    convertArray.push("I");
-    convert(num - 1);
-  }else {
-    return false;
+var convertArray = [];
+    if (num > 3999){
+      alert("Please enter a smaller number!")
+    } else if (num >= 1000){
+      convertArray.push("M");
+      convertArray += convert(num - 1000);
+    } else if (num >= 900){
+      convertArray.push("CM");
+      convertArray += convert(num - 900);
+    } else if (num >= 500){
+      convertArray.push("D");
+      convertArray += convert(num - 500);
+    } else if (num >= 400){
+      convertArray.push("CD");
+      convertArray += convert(num - 400);
+    } else if (num >= 100){
+      convertArray.push("C");
+    convertArray +=  convert(num - 100);
+    } else if (num >= 90){
+      convertArray.push("XC");
+    convertArray += convert(num - 90);
+    } else if (num >= 50){
+      convertArray.push("L");
+    convertArray +=  convert(num - 50);
+    } else if (num >= 40){
+      convertArray.push("XL");
+    convertArray +=  convert(num - 40);
+    } else if (num >= 10){
+      convertArray.push("X");
+    convertArray +=  convert(num - 10);
+    } else if (num >= 9){
+      convertArray.push("IX");
+    convertArray +=  convert(num - 9);
+    } else if (num >= 5){
+      convertArray.push("V");
+    convertArray +=  convert(num - 5);
+    } else if (num >= 4){
+      convertArray.push("IV");
+  convertArray +=    convert(num - 4);
+    }else if (num >= 1){
+      convertArray.push("I");
+    convertArray +=  convert(num - 1);
+    }
+    var output = convertArray.toString();
+    return output;
   }
-  console.log(convertArray.toString());
-}
   //UI Logic
-
+  $(function(){
   $("form#romannumerals").submit(function(event){
     event.preventDefault();
     var romanNumbers = parseInt($("input#numbers").val());
     var nums = convert(romanNumbers);
-    // var join = convertArray.join("");
-    // var string = convertArray.toString();
-    $("#nums").text(convertArray.join("").toString());
+    $("#nums").text(nums);
   });
  });
-
-
-
-
-//   / Frontend
-// $("form").submit(function(event){
-//   event.preventDefault();
-//   inputNumber = parseInt($("#inputNumber").val());
-//   convert(inputNumber);
-//   $("#result").text(outputArray.join("").toString());
-// });
